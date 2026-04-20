32db47fb66b546876de94d60692487dcd33cc110
mohonovproduction <mohonovproduction@yandex.ru>
2026-04-20 22:13:20 +0300

    first commit

---
+++ scrip.js
@@ -0,0 +1,22 @@
+// Main JavaScript
+
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM is ready');
+    
+    // Initialize your application here
+    init();
+});
+
+// Main initialization function
+function init() {
+    // Your code here
+}
+
+// Utility functions
+const utils = {
+    // Add your utility functions here
+    log: (message) => {
+        console.log(message);
+    }
+};
